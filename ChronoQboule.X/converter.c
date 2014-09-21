92d4c720ccc0bcec1ab7e8bf0c4286e5374f8cd0
RobinN7 <robin.beilvert@gmail.com>
2014-09-21 18:04:09 +0200

    Add project to folder

---
+++ ChronoQboule.X/converter.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <p18f25k80.h>
+#include "converter.h"
+
+void segments(char figure) {
+
+/*
+    0= a b c d e f
+    1= b c
+    2= a b d e g;
+    3= a b c d g;    
+    4= b c f g;            
+    5= a c d f g;
+    6= a c d e f g;
+    7= a b c;    
+    8= a b c d e f g;   
+    9= a b c d f g;
+*/
+            
+switch(figure)
+    {
+        case 0 :    PORTB =0b1111110; break;
+        case 1 :    PORTB =0b0110000; break;
+        case 2 :    PORTB =0b1101101; break;
+        case 3 :    PORTB =0b1111001; break;
+        case 4 :    PORTB =0b0110011; break;
+        case 5 :    PORTB =0b1011011; break;
+        case 6 :    PORTB =0b1011111; break;
+        case 7 :    PORTB =0b1110000; break;
+        case 8 :    PORTB =0b1111111; break;
+        case 9 :    PORTB =0b1111011; break;
+        default : ;
+    }    
+
+}
---
+++ ChronoQboule.X/converter.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   converter.h
+ * Author: Robin
+ *
+ * Created on 21 septembre 2014, 16:48
+ */
+
+#ifndef CONVERTER_H
+#define	CONVERTER_H
+
+#define segmentA PORTBbits_t.PORTB7
+#define segmentB PORTBbits_t.PORTB6
+#define segmentC PORTBbits_t.PORTB5
+#define segmentD PORTBbits_t.PORTB4
+#define segmentE PORTBbits_t.PORTB3
+#define segmentF PORTBbits_t.PORTB2
+#define segmentG PORTBbits_t.PORTB1
+#define segmentH PORTBbits_t.PORTB0
+
+
+void segments(char);
+
+
+#endif	/* CONVERTER_H */
